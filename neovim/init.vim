ec9aa206cb1793569b36963fb76e80fd8a56ae76
Gabriel Sanches <gabriel@gsr.dev>
2020-11-17 17:29:24 -0300

    neovim: move settings to Lua init file

--- neovim/init.vim
+++ neovim/init.vim
@@ -1,38 +1,8 @@
 let $GIT_EDITOR = "nvr -cc tabnew --remote-wait +'setlocal bufhidden=wipe'"
 let $VISUAL = "nvr -cc split --remote-wait +'setlocal bufhidden=wipe'"
 
-lua require('init')
-
 syntax on
 
-set nocompatible
-set noshowmode
-set number
-set laststatus=2
-set showtabline=2
-set signcolumn=yes:1
-set relativenumber
-set pumheight=10
-set title
-set incsearch
-set inccommand=split
-set completeopt=menuone,noinsert,noselect
-set shortmess+=c
-set diffopt+=algorithm:patience,indent-heuristic,iwhiteall,hiddenoff
-set backspace=2
-set virtualedit=block
-set expandtab
-set autoindent
-set tabstop=4
-set shiftwidth=2
-set colorcolumn=100
-set wrap
-set linebreak
-set list lcs+=tab:‣\ ,eol:↴,trail:␣
-set timeoutlen=500
-set splitright
-set splitbelow
-set hidden
-set foldlevel=99
+lua require('init')
 
 autocmd TermOpen * setlocal nospell
--- neovim/lua/init.lua
+++ neovim/lua/init.lua
@@ -8,6 +8,38 @@ require('config.lsp')
 require('config.syntax')
 require('config.utils')
 
+vim.o.compatible = false
+vim.o.showmode = false
+vim.o.number = true
+vim.o.laststatus = 2
+vim.o.showtabline = 2
+vim.o.signcolumn = 'yes:1'
+vim.o.relativenumber = true
+vim.o.pumheight = 10
+vim.o.title = true
+vim.o.incsearch = true
+vim.o.inccommand = 'split'
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.shortmess = 'c'
+vim.o.diffopt = 'algorithm:patience,indent-heuristic,iwhiteall,hiddenoff'
+vim.o.backspace = '2'
+vim.o.virtualedit = 'block'
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 2
+vim.o.colorcolumn = '100'
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.list = true
+vim.o.listchars = 'tab:‣ ,eol:↴,trail:␣'
+vim.o.timeoutlen = 500
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.hidden = true
+vim.o.foldlevel = 99
+
+
 -- TODO: config.git
 require('gitsigns').setup({
   signs = {
