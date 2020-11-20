3a5b18865cb0cef0f6a0ea3126a3ec7e6131a36a
Gabriel Sanches <gabriel@gsr.dev>
2020-11-19 21:38:11 -0300

    neovim: fix setting options using Lua

--- neovim/lua/init.lua
+++ neovim/lua/init.lua
@@ -11,35 +11,35 @@ require('config.git')
 
 vim.o.compatible = false
 vim.o.showmode = false
-vim.o.number = true
 vim.o.laststatus = 2
 vim.o.showtabline = 2
-vim.o.signcolumn = 'yes:1'
-vim.o.relativenumber = true
 vim.o.pumheight = 10
 vim.o.title = true
 vim.o.incsearch = true
 vim.o.inccommand = 'split'
-vim.o.completeopt = 'menuone,noinsert,noselect'
-vim.o.shortmess = 'c'
-vim.o.diffopt = 'algorithm:patience,indent-heuristic,iwhiteall,hiddenoff'
+vim.o.completeopt = vim.o.completeopt .. ',menuone,noinsert,noselect'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.diffopt = vim.o.diffopt .. ',algorithm:patience,indent-heuristic,iwhiteall,hiddenoff'
 vim.o.backspace = '2'
 vim.o.virtualedit = 'block'
 vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 2
-vim.o.colorcolumn = '100'
 vim.o.wrap = true
 vim.o.linebreak = true
 vim.o.list = true
-vim.o.listchars = 'tab:‣ ,eol:↴,trail:␣'
+vim.o.listchars = vim.o.listchars .. ',tab:  ,eol:↴,trail:␣'
 vim.o.timeoutlen = 500
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.hidden = true
 vim.o.foldlevel = 99
 
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes:1'
+vim.wo.colorcolumn = '100'
 
 vim.cmd [[augroup TreeSitterFolds]]
 vim.cmd   [[autocmd BufEnter * setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()]]
