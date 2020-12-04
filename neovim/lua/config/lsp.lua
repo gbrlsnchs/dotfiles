caf0fab314ce97057617b866c0dae806aedb6a3f
Gabriel Sanches <gabriel@gsr.dev>
2020-12-04 09:54:21 -0300

    neovim: Toggle LSP diagnostics signs on

--- neovim/lua/config/lsp.lua
+++ neovim/lua/config/lsp.lua
@@ -66,7 +66,7 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
     virtual_text = false,
-    signs = false,
+    signs = true,
   }
 )
 
