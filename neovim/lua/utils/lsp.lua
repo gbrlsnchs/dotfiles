698a209d2b86dc5a0e47a8db0e67914e2226125b
Gabriel Sanches <gabriel@gsr.dev>
2021-02-16 18:14:39 -0300

    neovim: Add LSP util to return Lua command string

--- neovim/lua/utils/lsp.lua
+++ neovim/lua/utils/lsp.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local is_disabled = os.getenv('NVIM_DISABLE_LSP')
 
+M.cmd_name = function(feat)
+  return string.format('<Cmd>lua vim.lsp.buf.%s()<CR>', feat)
+end
+
 M.is_enabled = function()
   return not is_disabled
 end
