80ae4e73275323bd34fa963eba9ce463ba35768a
Gabriel Sanches <gabriel@gsr.dev>
2021-10-28 21:03:54 -0300

    neovim: Format code

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,4 +2,5 @@ require("opt")
 require("deps")
 
 vim.env.GIT_EDITOR = "nvr -cc tabnew --remote-wait +'setlocal bufhidden=wipe'"
-vim.env.GIT_PAGER = "nvr -cc tabnew --remote-wait +'Man! | setlocal bufhidden=wipe number relativenumber | setfiletype git' -"
+vim.env.GIT_PAGER =
+	"nvr -cc tabnew --remote-wait +'Man! | setlocal bufhidden=wipe number relativenumber | setfiletype git' -"
