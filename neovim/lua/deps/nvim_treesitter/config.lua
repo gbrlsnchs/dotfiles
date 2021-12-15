e2c2c3db87e9e54cae99276727a59bb6eb16e990
Gabriel Sanches <gabriel@gsr.dev>
2021-12-15 17:47:14 -0300

    neovim: Update code to use Lua

--- neovim/lua/deps/nvim_treesitter/config.lua
+++ neovim/lua/deps/nvim_treesitter/config.lua
@@ -49,6 +49,5 @@ return function()
 	})
 
 	vim.opt.foldmethod = "expr"
-	-- vim.opt.foldexpr = vim.fn["nvim_treesitter#foldexpr"]()
-	vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
+	vim.opt.foldexpr = vim.fn["nvim_treesitter#foldexpr"]()
 end
