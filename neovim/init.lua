5a97a072402b46023bf101e0416aa7bebce0399d
Gabriel Sanches <gabriel@gsr.dev>
2021-12-15 18:42:11 -0300

    neovim: Use custom colorscheme and status line

--- neovim/init.lua
+++ neovim/init.lua
@@ -4,3 +4,17 @@ vim.env.GIT_PAGER =
 	"nvr -cc tabnew --remote-wait +'Man! | setlocal bufhidden=wipe number relativenumber | setfiletype git' -"
 
 vim.notify = require("internal.notify")
+
+vim.opt.background = "dark"
+vim.cmd("colorscheme custom")
+
+vim.cmd([[
+augroup status_line
+	autocmd!
+	autocmd WinEnter,BufEnter * setlocal statusline=%!v:lua.require('local.statusline').get_statusline('active')
+	autocmd WinLeave,BufLeave * setlocal statusline=%!v:lua.require('local.statusline').get_statusline('inactive')
+augroup END
+]])
+
+require("opt")
+require("deps")
