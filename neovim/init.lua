3ec25acdf76f8b5833fe0250d7f4a43a9c8204d6
Gabriel Sanches <gabriel@gsr.dev>
2021-12-15 17:49:51 -0300

    neovim: Set EDITOR env variable

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,6 +1,4 @@
-require("opt")
-require("deps")
-
+vim.env.EDITOR = "nvr --remote-wait"
 vim.env.GIT_EDITOR = "nvr -cc tabnew --remote-wait +'setlocal bufhidden=wipe'"
 vim.env.GIT_PAGER =
 	"nvr -cc tabnew --remote-wait +'Man! | setlocal bufhidden=wipe number relativenumber | setfiletype git' -"
