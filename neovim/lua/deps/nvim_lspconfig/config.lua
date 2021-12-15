58b5ec1e957ce90201a08aa5d4e57049aff1e99e
Gabriel Sanches <gabriel@gsr.dev>
2021-12-15 18:37:45 -0300

    neovim: Show borders for some LSP stuff

--- neovim/lua/deps/nvim_lspconfig/config.lua
+++ neovim/lua/deps/nvim_lspconfig/config.lua
@@ -126,11 +126,14 @@ return function()
 		float = { border = "single" },
 	})
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = nil })
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+		vim.lsp.handlers.hover,
+		{ border = "single" }
+	)
 
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 		vim.lsp.handlers.signature_help,
-		{ border = nil }
+		{ border = "single" }
 	)
 
 	local efm_formatters = {
