801f0038fb44b4352bed8ecaaf911f88d0f03ce3
Gabriel Sanches <gabriel@gsr.dev>
2021-12-15 18:33:04 -0300

    zsh: Update prompt glyphs

--- zsh/zdotdir/zshrc.d/10-prompt.zsh
+++ zsh/zdotdir/zshrc.d/10-prompt.zsh
@@ -5,6 +5,6 @@ _git_branch() {
 	fi
 }
 
-PROMPT='%F{$color6}%B%d%b%f%F{$color8}$(_git_branch)%f
-%F{$color8}%B>%b%f '
+PROMPT='%F{$color7}┌%f %F{$color6}%B%d%b%f%F{$color7}$(_git_branch)%f
+%F{$color7}└%f '
 RPROMPT='%(1j.jobs: %j, .)%(0?.%F{$color10}OK.%F{$color9}ERR [%?])%f'
