1819682b8f04bbb120613040c152dcb18dad32d6
Gabriel Sanches <gabriel@gsr.dev>
2022-01-01 20:10:35 -0300

    neovim: Refactor initialization log message

--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -3,17 +3,19 @@ local logger = require("lib.logger")
 local function load_mods(dir, mods, allowlist)
 	allowlist = allowlist or {}
 
+	local total = 0
 	for _, mod in ipairs(mods) do
 		local mod_path = ("%s.%s"):format(dir, mod)
 		local ok, err = pcall(require, mod_path)
 		if ok then
 			logger.debugf("Initialized %q", mod_path)
+			total = total + 1
 		else
 			logger.errorf("Unable to require %q: %s", mod_path, err)
 		end
 	end
 
-	logger.infof("Loaded all modules from %q", dir)
+	logger.infof("Total modules loaded from %q: %d/%d", dir, total, #mods)
 end
 
 load_mods("opts", {
