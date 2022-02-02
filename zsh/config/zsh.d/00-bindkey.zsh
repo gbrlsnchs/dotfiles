364eae6bacf077f3690a686c7eccf41bc3f2949b
Gabriel Sanches <gabriel@gsr.dev>
2022-02-01 21:28:28 -0300

    zsh: Add more useful keybindings

--- zsh/config/zsh.d/00-bindkey.zsh
+++ zsh/config/zsh.d/00-bindkey.zsh
@@ -1,7 +1,10 @@
 bindkey -v # This enables Vim mode.
 
 # History navigation.
-bindkey "^P" up-line-or-history
-bindkey "^N" down-line-or-history
-bindkey "^[[A" up-line-or-search
-bindkey "^[[B" down-line-or-search
+bindkey "^P" up-line-or-history    # Ctrl-P
+bindkey "^N" down-line-or-history  # Ctrl-N
+bindkey "^[[A" up-line-or-search   # Arrow up
+bindkey "^[[B" down-line-or-search # Arrow down
+bindkey "^[[H" beginning-of-line   # Home
+bindkey "^[[F" end-of-line         # End
+bindkey "^[[3~" delete-char        # Delete
