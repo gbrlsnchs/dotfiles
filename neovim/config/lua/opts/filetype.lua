926b51490ed7ff27715ad19cf2c5022a7f0e50b3
Gabriel Sanches <gabriel@gsr.dev>
2022-03-24 13:49:57 -0300

    neovim: Set up some file types

--- neovim/config/lua/opts/filetype.lua
+++ neovim/config/lua/opts/filetype.lua
@@ -1,3 +1,20 @@
 -- NOTE: Remove these after support for filetype.lua is official.
 vim.g.did_load_filetypes = 0
 vim.g.do_filetype_lua = 1
+
+vim.filetype.add({
+	extension = {
+		gcfg = "dosini",
+		just = "just",
+	},
+	literals = {
+		Justfile = "just",
+		justfile = "just",
+		[".Justfile"] = "just",
+		[".justfile"] = "just",
+	},
+	pattern = {
+		-- Void Linux templates.
+		[".*/srcpkgs/.*/template"] = "sh",
+	},
+})
