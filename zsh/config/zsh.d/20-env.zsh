7511ce3ae3ad6ec50ed7cc1eea239c46f27bc7f8
Gabriel Sanches <gabriel@gsr.dev>
2022-06-07 21:33:33 -0300

    zsh: Set up Rust/Cargo env variables

--- zsh/config/zsh.d/20-env.zsh
+++ zsh/config/zsh.d/20-env.zsh
@@ -1,5 +1,7 @@
 # cargo
-path+=("$HOME/.cargo/bin")
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+path+=("$CARGO_HOME/bin")
 
 # go
 path+=("$(go env GOPATH)/bin")
