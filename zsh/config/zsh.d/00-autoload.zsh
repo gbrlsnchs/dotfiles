1e3750e9b8381e1e165ef07741ba50825c3d80ce
Gabriel Sanches <gabriel@gsr.dev>
2022-06-07 21:32:41 -0300

    zsh: Refactor by quoting expressions

--- zsh/config/zsh.d/00-autoload.zsh
+++ zsh/config/zsh.d/00-autoload.zsh
@@ -4,6 +4,6 @@ autoload -Uz compinit && compinit
 fpath+=("$XDG_DATA_HOME/zsh/zfunc" "$ZDOTDIR/functions")
 typeset -U fpath
 
-for func in $ZDOTDIR/functions/* ; do
-	autoload -U $(basename $func)
+for func in "$ZDOTDIR"/functions/* ; do
+	autoload -Uz "$(basename $func)"
 done
