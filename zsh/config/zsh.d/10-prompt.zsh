94b921d708ea8ae355dde2d74ab45cbacf461141
Gabriel Sanches <gabriel@gsr.dev>
2022-06-21 17:52:20 -0300

    zsh: Update ZSH prompt

--- zsh/config/zsh.d/10-prompt.zsh
+++ zsh/config/zsh.d/10-prompt.zsh
@@ -1,10 +1,10 @@
 _git_branch() {
 	local branch_name=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-	if [[ $branch_name != '' ]]; then
-		echo " ($branch_name)"
+	if [[ "$branch_name" != "" ]]; then
+		echo "(${branch_name}枝)"
 	fi
 }
 
-PROMPT='%F{$color7}┌%f %F{$color6}%B%d%b%f%F{$color7}$(_git_branch)%f
-%F{$color7}└%f '
+PROMPT='%F{$color12}%B%n@%m%b%f %F{$color6}%B%~%b%f %F{$color7}$(_git_branch)%f 
+%F{$color8}%B>>>%b%f '
 RPROMPT='%(1j.jobs: %j, .)%(0?.%F{$color10}OK.%F{$color9}ERR [%?])%f'
