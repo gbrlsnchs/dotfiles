0c209382b71780c16851063282916de9ce09f7ec
Gabriel Sanches <gabriel@gsr.dev>
2022-08-18 16:48:54 -0300

    zsh: Use 'kana' to print hostname

--- zsh/config/zsh.d/10-prompt.zsh
+++ zsh/config/zsh.d/10-prompt.zsh
@@ -5,6 +5,10 @@ _git_branch() {
 	fi
 }
 
-PROMPT='%F{$color12}%B%n@%m%b%f %F{$color6}%B%~%b%f %F{$color7}$(_git_branch)%f 
+_kana_hostname() {
+	kana --katakana $(hostname) 2>/dev/null || echo "%m"
+}
+
+PROMPT='%F{$color12}%B%n@$(_kana_hostname)%b%f %F{$color6}%B%~%b%f %F{$color7}$(_git_branch)%f 
 %F{$color8}%B>>>%b%f '
 RPROMPT='%(1j.jobs: %j, .)%(0?.%F{$color10}OK.%F{$color9}ERR [%?])%f'
