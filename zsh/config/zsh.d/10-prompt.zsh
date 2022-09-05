fa49c0c37073e40a5982716206fed54d75a1ed66
Gabriel Sanches <gabriel@gsr.dev>
2022-09-05 20:20:56 -0300

    zsh: Update prompt colors

--- zsh/config/zsh.d/10-prompt.zsh
+++ zsh/config/zsh.d/10-prompt.zsh
@@ -9,6 +9,6 @@ _kana_hostname() {
 	kana --katakana $(hostname) 2>/dev/null || echo "%m"
 }
 
-PROMPT='%F{$color12}%B%n@$(_kana_hostname)%b%f %F{$color6}%B%~%b%f %F{$color7}$(_git_branch)%f 
-%F{$color8}%B>>>%b%f '
-RPROMPT='%(1j.jobs: %j, .)%(0?.%F{$color10}OK.%F{$color9}ERR [%?])%f'
+PROMPT='%F{#$COLOR_12}%B%n@$(_kana_hostname)%b%f %F{#$COLOR_6}%B%~%b%f %F{#$COLOR_7}$(_git_branch)%f 
+%F{#$COLOR_8}%B>>>%b%f '
+RPROMPT='%(1j.jobs: %j, .)%(0?.%F{#$COLOR_10}OK.%F{#$COLOR_9}ERR [%?])%f'
