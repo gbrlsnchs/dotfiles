1ceb448bce13ef6025acc485dc57418548f6008c
Gabriel Sanches <gabriel@gsr.dev>
2022-09-05 20:24:52 -0300

    zsh: Change plugin for FZF completion

--- zsh/config/zsh.d/20-fuzzy.zsh
+++ zsh/config/zsh.d/20-fuzzy.zsh
@@ -15,10 +15,10 @@ for file in /usr/share/fzf/*.zsh ; do
 	source $file
 done
 
-load_plugin fzf-tab
+load_plugin fzf-tab-completion zsh/fzf-zsh-completion.sh
+bindkey '^I' fzf_completion
+zstyle ':completion:*' fzf-search-display true
 
-zstyle ':fzf-tab:*' fzf-flags \
-    --color fg:"$foreground",bg:"$background",hl:"$color11" \
-	--color fg+:"$foreground",bg+:"$background",hl+:"$color3" \
-    --color info:"$foreground",prompt:"$color2",pointer:"$foreground" \
-	--color spinner:"$foreground",marker:"$foreground"
+if [[ -o login ]]; then
+	export FZF_DEFAULT_OPTS=""
+fi
