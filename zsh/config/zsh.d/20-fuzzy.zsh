623570577686ff05ebbd8449af1793962a80d84b
Gabriel Sanches <gabriel@gsr.dev>
2022-09-05 20:21:44 -0300

    zsh: Skip setting FZF colors if using login shell

--- zsh/config/zsh.d/20-fuzzy.zsh
+++ zsh/config/zsh.d/20-fuzzy.zsh
@@ -1,12 +1,15 @@
 export FZF_DEFAULT_COMMAND="fd --color=always --type file --hidden --no-ignore-vcs"
-export FZF_DEFAULT_OPTS="
-    $FZF_DEFAULT_OPTS
-	--ansi
-    --color fg:${foreground},bg:${background},hl:${color11}
-	--color fg+:${foreground},bg+:${background},hl+:${color3}
-    --color info:${foreground},prompt:${color2},pointer:${foreground}
-	--color spinner:${foreground},marker:${foreground}
-"
+
+if [[ ! -o login ]]; then
+	export FZF_DEFAULT_OPTS="
+		$FZF_DEFAULT_OPTS
+		--ansi
+		--color fg:#$FG_COLOR,bg:#$BG_COLOR,hl:#$COLOR_1
+		--color fg+:#$FG_COLOR,bg+:#$BG_COLOR,hl+:#$COLOR_9
+		--color info:#$FG_COLOR,prompt:#$COLOR_2,pointer:#$FG_COLOR
+		--color spinner:#$FG_COLOR,marker:#$FG_COLOR
+		"
+fi
 
 for file in /usr/share/fzf/*.zsh ; do
 	source $file
