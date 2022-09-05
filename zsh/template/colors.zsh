4cb58125c14e01fd0c6b5b6280ecd41cbcc6d4f9
Gabriel Sanches <gabriel@gsr.dev>
2022-09-05 20:26:05 -0300

    zsh: Export variables for colors and cursor style

--- zsh/template/colors.zsh
+++ zsh/template/colors.zsh
@@ -1,27 +1,30 @@
 # {{notice}}
-background='#{{background}}'
-foreground='#{{foreground}}'
+#
+export BG_COLOR="{{background}}"
+export FG_COLOR="{{foreground}}"
 
 # Colors
-color0='#{{color0}}'
-color1='#{{color1}}'
-color2='#{{color2}}'
-color3='#{{color3}}'
-color4='#{{color4}}'
-color5='#{{color5}}'
-color6='#{{color6}}'
-color7='#{{color7}}'
-color8='#{{color8}}'
-color9='#{{color9}}'
-color10='#{{color10}}'
-color11='#{{color11}}'
-color12='#{{color12}}'
-color13='#{{color13}}'
-color14='#{{color14}}'
-color15='#{{color15}}'
+export COLOR_0="{{color0}}"
+export COLOR_1="{{color1}}"
+export COLOR_2="{{color2}}"
+export COLOR_3="{{color3}}"
+export COLOR_4="{{color4}}"
+export COLOR_5="{{color5}}"
+export COLOR_6="{{color6}}"
+export COLOR_7="{{color7}}"
+export COLOR_8="{{color8}}"
+export COLOR_9="{{color9}}"
+export COLOR_10="{{color10}}"
+export COLOR_11="{{color11}}"
+export COLOR_12="{{color12}}"
+export COLOR_13="{{color13}}"
+export COLOR_14="{{color14}}"
+export COLOR_15="{{color15}}"
 
 # Fix LS_COLORS being unreadable.
 export LS_COLORS="${LS_COLORS}:su=30;41:ow=30;42:st=30;44:"
 
 # Fix jq colors being unreadable.
 export JQ_COLORS="0;31:0;39:0;39:0;39:0;32:1;39:1;39"
+
+export XCURSOR_THEME="{{cursor_theme}}"
