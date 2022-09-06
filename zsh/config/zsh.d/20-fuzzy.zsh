364e83af76cbd5c25fd733765f59eeb929507628
Gabriel Sanches <gabriel@gsr.dev>
2022-09-06 17:45:52 -0300

    zsh: Go back to previous FZF tab completion plugin

--- zsh/config/zsh.d/20-fuzzy.zsh
+++ zsh/config/zsh.d/20-fuzzy.zsh
@@ -15,10 +15,12 @@ for file in /usr/share/fzf/*.zsh ; do
 	source $file
 done
 
-load_plugin fzf-tab-completion zsh/fzf-zsh-completion.sh
-bindkey '^I' fzf_completion
-zstyle ':completion:*' fzf-search-display true
+load_plugin fzf-tab
 
-if [[ -o login ]]; then
-	export FZF_DEFAULT_OPTS=""
+if [[ ! -o login ]]; then
+	zstyle ':fzf-tab:*' fzf-flags \
+		--color "fg:#$FG_COLOR,bg:#$BG_COLOR,hl:#$COLOR_1" \
+		--color "fg+:#$FG_COLOR,bg+:#$BG_COLOR,hl+:#$COLOR_9" \
+		--color "info:#$FG_COLOR,prompt:#$COLOR_2,pointer:#$FG_COLOR" \
+		--color "spinner:#$FG_COLOR,marker:#$FG_COLOR"
 fi
