8f3d8335b316fc64225a4f5126830d6f8f956bff
Gabriel Sanches <gabriel@gsr.dev>
2022-10-02 11:55:07 -0300

    neovim: Use native libluv API for exec

--- neovim/config/lua/me/api/exec.lua
+++ neovim/config/lua/me/api/exec.lua
@@ -1,24 +1,53 @@
-local Job = require("plenary.job")
+local loop = vim.loop
 
 local M = {}
+
+local function handle(tbl)
+	return function(err, data)
+		if err then
+			vim.notify(err, vim.log.levels.ERROR)
+			return
+		end
+
+		local lines = vim.split(data or "", "\n")
+		for _, l in ipairs(lines) do
+			if l ~= "" then
+				table.insert(tbl, l)
+			end
+		end
+	end
+end
 
 --- Executes a non-interactive program and pass response to a callback.
 --- @param cmd string: Command to be executed.
 --- @param args table: List of arguments passed to the command.
 --- @param callback function: Handler for the command, receives response and exit code.
 function M.cmd(cmd, args, callback)
-	local job = Job:new({
-		command = cmd,
-		args = args,
-		interactive = false,
-		on_exit = vim.schedule_wrap(function(job, exit_code)
-			local result = job:result()
+	local stdout = loop.new_pipe(false)
+	local stderr = loop.new_pipe(false)
+	local results = {}
+	local proc
 
-			callback(result, exit_code)
-		end),
-	})
+	proc = loop.spawn(
+		cmd,
+		{
+			args = args,
+			stdio = { nil, stdout, stderr },
+		},
+		vim.schedule_wrap(function(exit_code)
+			stdout:read_stop()
+			stderr:read_stop()
 
-	job:start()
+			stdout:close()
+			stderr:close()
+
+			proc:close()
+
+			callback(results, exit_code)
+		end)
+	)
+	loop.read_start(stdout, handle(results))
+	loop.read_start(stderr, handle(results))
 end
 
 return M
