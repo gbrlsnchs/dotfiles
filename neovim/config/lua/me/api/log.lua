b7bf517083a6db342c18c7e0eb5d779117cd5a68
Gabriel Sanches <gabriel@gsr.dev>
2022-10-02 12:12:02 -0300

    neovim: Export log previewing function

--- neovim/config/lua/me/api/log.lua
+++ neovim/config/lua/me/api/log.lua
@@ -112,4 +112,26 @@ function M.reset_unread()
 	unread_severity = 0
 end
 
+function M.preview()
+	-- HACK: This forces the preview to open when current window doesn't have a file name.
+	local tmp = vim.fn.tempname()
+	vim.cmd.pedit(tmp)
+
+	local preview_win = win.get_preview()
+	local logs_buffer = M.get_buffer()
+
+	api.nvim_win_set_buf(preview_win, logs_buffer)
+	api.nvim_win_set_option(preview_win, "number", false)
+	api.nvim_win_set_option(preview_win, "relativenumber", false)
+	api.nvim_win_set_option(preview_win, "list", false)
+	api.nvim_win_call(preview_win, function()
+		vim.cmd("wincmd J | normal G")
+	end)
+
+	vim.cmd.bdelete(tmp)
+
+	M.reset_unread()
+end
+
+
 return M
--- neovim/config/lua/me/cfg/editor/misc.lua
+++ neovim/config/lua/me/cfg/editor/misc.lua
@@ -6,27 +6,6 @@ local win = require("me.api.win")
 
 local api = vim.api
 
-local function preview_logs()
-	-- HACK: This forces the preview to open when current window doesn't have a file name.
-	local tmp = vim.fn.tempname()
-	vim.cmd("pedit " .. tmp)
-
-	local preview_win = win.get_preview()
-	local logs_buffer = log.get_buffer()
-
-	api.nvim_win_set_buf(preview_win, logs_buffer)
-	api.nvim_win_set_option(preview_win, "number", false)
-	api.nvim_win_set_option(preview_win, "relativenumber", false)
-	api.nvim_win_set_option(preview_win, "list", false)
-	api.nvim_win_call(preview_win, function()
-		vim.cmd("wincmd J | normal G")
-	end)
-
-	vim.cmd("bdelete " .. tmp)
-
-	log.reset_unread()
-end
-
 local function copy_buf_path()
 	local winid, bufnr = winpick.select({
 		format_label = winpick.defaults.format_label,
@@ -61,7 +40,7 @@ excmd.register("Utils", {
 	Logs = {
 		desc = "Show Neovim logs in preview window",
 		callback = function()
-			preview_logs()
+			log.preview()
 		end,
 	},
 	CopyBufPath = {
