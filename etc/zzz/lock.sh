e66b2f2dfa5357bb7b7918b9fa029adf84b0aa1a
Gabriel Sanches <gabriel@gsr.dev>
2023-05-16 16:45:12 -0300

    etc/zzz: Add suspend hook

---
+++ etc/zzz/lock.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo --preserve-env --user "$(id --user --name 1000)" waylock \
+	-fork-on-lock \
+	-init-color "0x${BG_COLOR}" \
+	-input-color "0x${COLOR_4}" \
+	-fail-color "0x${COLOR_1}"
