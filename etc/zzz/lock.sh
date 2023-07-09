19a82b84ef884d251ee59538d39f422b8e45e31a
Gabriel Sanches <gabriel@gsr.dev>
2023-07-09 19:02:20 -0300

    etc/zzz: Change background color

--- etc/zzz/lock.sh
+++ etc/zzz/lock.sh
@@ -2,6 +2,6 @@
 
 sudo --preserve-env --user "$(id --user --name 1000)" waylock \
 	-fork-on-lock \
-	-init-color "0x${BG_COLOR}" \
+	-init-color "0x${FG_COLOR}" \
 	-input-color "0x${COLOR_4}" \
 	-fail-color "0x${COLOR_1}"
