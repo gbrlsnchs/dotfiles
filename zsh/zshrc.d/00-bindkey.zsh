5e5958056d4e659fd735572c1baa3ad16af1be4c
Gabriel Sanches <gabriel@gsr.dev>
2023-10-08 19:03:52 -0300

    zsh: Fix Ctrl-W behavior
    This was so annoying.

--- zsh/zshrc.d/00-bindkey.zsh
+++ zsh/zshrc.d/00-bindkey.zsh
@@ -1,11 +1,18 @@
 # bindkey -v # This enables Vim mode.
 bindkey -e
 
+my-backward-delete-word() {
+	local WORDCHARS=${WORDCHARS/\//}
+	zle backward-delete-word
+}
+zle -N my-backward-delete-word
+
 # History navigation.
-bindkey "^P" up-line-or-history    # Ctrl-P
-bindkey "^N" down-line-or-history  # Ctrl-N
-bindkey "^[[A" up-line-or-search   # Arrow up
-bindkey "^[[B" down-line-or-search # Arrow down
-bindkey "^[[H" beginning-of-line   # Home
-bindkey "^[[F" end-of-line         # End
-bindkey "^[[3~" delete-char        # Delete
+bindkey "^W" my-backward-delete-word # Ctrl-W
+bindkey "^P" up-line-or-history      # Ctrl-P
+bindkey "^N" down-line-or-history    # Ctrl-N
+bindkey "^[[A" up-line-or-search     # Arrow up
+bindkey "^[[B" down-line-or-search   # Arrow down
+bindkey "^[[H" beginning-of-line     # Home
+bindkey "^[[F" end-of-line           # End
+bindkey "^[[3~" delete-char          # Delete
