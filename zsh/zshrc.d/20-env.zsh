4fcc1784f6cca1a02110b0ff8c4f65195dedfb1a
Gabriel Sanches <gabriel@gsr.dev>
2023-10-17 01:11:47 -0300

    zsh: Prefer local bin path

--- zsh/zshrc.d/20-env.zsh
+++ zsh/zshrc.d/20-env.zsh
@@ -3,7 +3,7 @@ path=("$XDG_BIN_HOME" $path)
 # cargo
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
-path+=("$CARGO_HOME/bin")
+path=("$CARGO_HOME/bin" $path)
 
 # node
 export NEXT_TELEMETRY_DISABLED=1
@@ -11,17 +11,17 @@ export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # go
-path+=("$(go env GOPATH)/bin")
+path=("$(go env GOPATH)/bin" $path)
 export GOPROXY=direct
 
 # python
-path+=("$HOME/.poetry/bin")
+path=("$HOME/.poetry/bin" $path)
 
 # latex
-path+=("/opt/texlive/2021/bin/$(uname --machine)-$(uname --kernel-name | tr '[:upper:]' '[:lower:]')")
+path=("/opt/texlive/2021/bin/$(uname --machine)-$(uname --kernel-name | tr '[:upper:]' '[:lower:]')" $path)
 
 # ruby
-path+=("$HOME/.rvm/bin")
+path=("$HOME/.rvm/bin" $path)
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 export SVDIR="$XDG_DATA_HOME/service"
